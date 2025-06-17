53842e546dae5462e5ecc7e18677da25f3d3ff2e
mendi kuperman <mendikuperman@gmail.com>
2025-06-17 17:59:20 +0300

    feat: added the option to check if an istance of pulse sensor already was created and re-use it because if so we want to limit its use up to three times

--- Game/GameManeger.cs
+++ Game/GameManeger.cs
@@ -7,7 +7,6 @@ namespace Game
     public static class GameManeger
     {
         public static void Start()
-
         {
             SimpleAgent agent = new SimpleAgent("mac");
 
@@ -16,36 +15,31 @@ namespace Game
             ISensore BSensore2 = Sensorfactory.CreateSensor("Audio");
             ISensore BSensore3 = Sensorfactory.CreateSensor("Pulse");
 
-
-            agent.AddWeakSensore(BSensore1);
-            agent.AddWeakSensore(BSensore2);
             agent.AddWeakSensore(BSensore3);
-
+            agent.AddWeakSensore(BSensore2);
+            agent.AddWeakSensore(BSensore1);
 
             int trueGuesses = 0;
 
-            //creating a copy of Weaknesses senssors list so we can remove sensor we already check to prevent duplication
+            // creating a copy of Weaknesses sensors list so we can remove matched sensors
             List<ISensore> leftToCheck = new List<ISensore>(agent.weaknessesSensors);
-
-
 
             while (trueGuesses < agent.weaknessesSensors.Count)
             {
-
                 Console.WriteLine("Enter your guess:");
                 string guess = Console.ReadLine();
 
-                //checks if Pulse sensor Already exist becuase if so we wnat to reuse it
-                ISensore existing = agent.AttachedSensors.FirstOrDefault(s => s.Type == guess && !s.IsBroken);
-
+                // Try to reuse existing sensor
+                ISensore existing = agent.AttachedSensors.FirstOrDefault(s => s.Type == guess);
                 ISensore sensore;
 
                 if (existing != null)
                 {
                     sensore = existing;
-                    Console.WriteLine(" Re-using existing sensor.");
-                }
 
+                    if (!existing.IsBroken)
+                        Console.WriteLine(" Re-using existing sensor.");
+                }
                 else
                 {
                     sensore = Sensorfactory.CreateSensor(guess);
@@ -53,46 +47,37 @@ namespace Game
                     Console.WriteLine(" New sensor created and attached.");
                 }
 
-
-
-
-
-                // loop through the weaknesses Sensors list to find a match between the gusse and one of the sensors
-
-
+                // If the sensor is broken (like Pulse after 3 activations), skip it
                 if (sensore.IsBroken)
                 {
                     Console.WriteLine(" This sensor is broken and was not activated.");
-                    break;//we found a broken sensor so we don't want to use it again 
+                    continue; // 
                 }
 
-                else
+                // Try to match with any remaining weakness
+                bool matchedThisTurn = false;
+
+                foreach (ISensore s in leftToCheck)
                 {
-                    foreach (ISensore s in leftToCheck)
+                    bool match = sensore.Activate(s);
+                    if (match)
                     {
-                        bool match = s.Activate(guess);
-                        if (match)
-                        {
-                            Console.WriteLine(" Match!");
-                            trueGuesses++;
-                            leftToCheck.Remove(s);
-                            break; //there is no need to check anymore
-                        }
-
+                        Console.WriteLine(" Match!");
+                        trueGuesses++;
+                        leftToCheck.Remove(s); // remove matched weakness
+                        matchedThisTurn = true;
+                        break;
                     }
-
                 }
-                   Console.WriteLine($"you guessed {trueGuesses}/{agent.weaknessesSensors.Count()} correct sensors ");
-                     Console.WriteLine();
 
+                if (!matchedThisTurn)
+                {
+                    Console.WriteLine(" No match this time.");
+                }
+
+                Console.WriteLine($"You guessed {trueGuesses}/{agent.weaknessesSensors.Count} correct sensors");
+                Console.WriteLine();
             }
-
-
-
-         
         }
-
-     
-        }
+    }
 }
-
