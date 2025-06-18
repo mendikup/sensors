194a562fd3636b648765fac7e1f07021dda5565c
mendi kuperman <mendikuperman@gmail.com>
2025-06-18 16:57:29 +0300

    added an Agent factory to create a  diffrent Agents

---
+++ Agents/AgentFactory.cs
@@ -0,0 +1,56 @@
+using sensore;
+
+namespace Agants
+{
+    public static class Agentfactory
+    {
+        public static Agent CreateNweAgent(string agentType)
+        {
+            switch (agentType)
+            {
+                case "foot soldier":
+
+                    var fooutSoldier = new Footsoldier();
+
+                    fooutSoldier.AddWeakSensore(Sensorfactory.CreateSensor("Pulse"));
+                    fooutSoldier.AddWeakSensore(Sensorfactory.CreateSensor("Audio"));
+
+                    return fooutSoldier;
+
+                    break;
+
+                case "squad leader":
+
+                    var squadLeader = new Squadleader();
+
+                    squadLeader.AddWeakSensore(Sensorfactory.CreateSensor("Pulse"));
+                    squadLeader.AddWeakSensore(Sensorfactory.CreateSensor("Audio"));
+                    squadLeader.AddWeakSensore(Sensorfactory.CreateSensor("Pulse"));
+                    squadLeader.AddWeakSensore(Sensorfactory.CreateSensor("Audio"));
+
+                    return squadLeader;
+                    break;
+
+                default:
+
+                    {
+                        var fooutSoldier1  = new Footsoldier();
+
+                        fooutSoldier1.AddWeakSensore(Sensorfactory.CreateSensor("Pulse"));
+                        fooutSoldier1.AddWeakSensore(Sensorfactory.CreateSensor("Audio"));
+
+                        return fooutSoldier1;
+
+
+                    }
+
+
+
+            }
+
+
+
+        }
+    }
+
+}
